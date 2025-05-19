fee54dcc2414521f26fb584d29872700eb978e20
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-05-20 00:28:29 +0500

    fix: fix database connection

--- internal/config/db/db.go
+++ internal/config/db/db.go
@@ -17,7 +17,7 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
